c6919d9a900673dc786cda9d52623095ce08847f
dddddanica <zhaodanica@gmail.com>
2024-12-13 01:13:33 +0000

    feat(3744): remove not necessary promise for `getMetaMetricsId` callback

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -2435,9 +2435,7 @@ export default class MetamaskController extends EventEmitter {
         allowedEvents: [],
       }),
       disabled: !this.preferencesController.state.useExternalServices,
-      getMetaMetricsId: Promise.resolve(
-        this.metaMetricsController.getMetaMetricsId(),
-      ),
+      getMetaMetricsId: () => this.metaMetricsController.getMetaMetricsId(),
       clientConfigApiService: new ClientConfigApiService({
         fetch: globalThis.fetch.bind(globalThis),
         config: {
